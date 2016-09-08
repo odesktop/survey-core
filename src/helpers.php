1c0a081ff08e6b9e8aae98c8f8a607aadc95f61b
primalbase <kawai@primalbase.jp>
2016-09-08 13:36:14 +0900

    typo修正、style指定を追加

--- src/helpers.php
+++ src/helpers.php
@@ -1,12 +1,13 @@
 <?php
 if (!function_exists('wkhtmltopdf')) {
 
-  function wkhtmltopdf($url, $filename)
+  function wkhtmltopdf($url, $filename, $style = null)
   {
     $config = Config::get('survey-core::wkhtmltopdf');
 
-    $default = array_get($config, 'default');
-    $command = array_get($config, 'commands.'.$default);
+    if (is_null($style))
+      $style = array_get($config, 'default');
+    $command = array_get($config, 'commands.'.$style);
     return sprintf('%s %s %s > /dev/null 2>&1',
       $command,
       $url,
